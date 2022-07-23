b275eb5fa9524602a7e21737289def15a0dd7c0c
zakiamansyah <zakiamansyah2014@gmail.com>
2022-07-23 14:33:29 +0700

    update route input data

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello I'm Here"
+    return render_template('index.html')
+
+@app.route('/inputData')
+def inputData():
+    return render_template('inputData.html')
