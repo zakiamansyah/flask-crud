77492b540d3fe7825e8a4ed5e63db0ca487a8517
zakiamansyah <zakiamansyah2014@gmail.com>
2022-07-28 03:54:11 +0700

    Add class users and hashing password

--- app/models.py
+++ app/models.py
@@ -1,4 +1,17 @@
 from app import db
+from werkzeug.security import check_password_hash, generate_password_hash
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+    
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
 
 class Employes(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
